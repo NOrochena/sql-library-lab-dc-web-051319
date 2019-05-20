6bfba343b7e43d8ecf5cf8705b528e0b5e735bca
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-19 23:42:31 -0400

    completed lab

---
+++ lib/schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE series (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    author_id integer,
+    subgenre_id integer
+);
+
+CREATE TABLE subgenres (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE authors (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE books (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    year integer,
+    series_id INTEGER
+);
+
+CREATE TABLE characters (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    motto TEXT,
+    species TEXT,
+    author_id INTEGER,
+    series_id INTEGER
+);
+
+CREATE TABLE character_books (
+    id INTEGER PRIMARY KEY,
+    character_id INTEGER,
+    book_id INTEGER
+);
+
+
